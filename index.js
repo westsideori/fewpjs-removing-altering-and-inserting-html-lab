3f08af904a5a5c7c444e31933edccab60a4c5bc5
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-10 21:34:00 -0500

    commit tree 84dff32645a7842b63254b852175ffd3bd812578

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const message = "hi"
+
+document.querySelector("main#main").remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Ori is the champion'
